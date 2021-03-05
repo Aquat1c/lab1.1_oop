6ee6662258a3d95292cb38f98a5bc3df4551d882
Aquat1c <49980958+Aquat1c@users.noreply.github.com>
2021-03-05 12:36:36 +0200

    Add files via upload

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../oop_lab_1.1/Goods.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Assert::AreEqual(1, (1));
+		}
+	};
+}
---
+++ oop_lab_1.1/Goods.h
@@ -0,0 +1,11 @@
+#pragma once
+class Goods
+{
+	double first;
+	int	second;
+public:
+	bool Init(double, int);
+	void Read();
+	void Display() const;
+	double Cost() const;
+};
